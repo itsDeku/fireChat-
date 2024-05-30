7af6a95a281f2e0a32a2e92fc4b1c0ae63dd840c
itsDeku <deepaksaini63871@gmail.com>
2024-05-31 02:50:17 +0530

    removed strict

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import {RouterProvider} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React>
     <RouterProvider router={MyRouter} />
-  </React>
 );
 
